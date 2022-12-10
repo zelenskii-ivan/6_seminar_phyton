abd283d7119f4d8e51aa36fc4853530da9127a41
zelenskii-ivan <112585234+zelenskii-ivan@users.noreply.github.com>
2022-12-10 23:49:23 +0300

    Update 1_task.py

--- 1_task.py
+++ 1_task.py
@@ -9,15 +9,6 @@
 my_list = [2, 3, 4, 5, 6]
 res_list = []
 half_steps = len(my_list) // 2
-
-# Было
-# if len(my_list) % 2 != 0:
-#     half_steps += 1
-# for i in range(half_steps):
-#     res_list.append(my_list[i] * my_list[-i - 1])
-
-# Стало
-
 if len(my_list) % 2 != 0:
     half_steps += 1
 res_list = [my_list[x] * my_list[-x - 1] for x in range(half_steps)]
